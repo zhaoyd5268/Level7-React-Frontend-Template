ff44934a93a2a2b07a160503ecdb74c9b6d9d1af
Anurup Joseph <anurup.joseph@jointheleague.org>
2020-10-21 18:27:00 -0400

    Element creation.

---
+++ src/components/SearchOutput/SearchResult/SearchResult.js
@@ -0,0 +1,19 @@
+import React from "react";
+import classes from './SearchResult.module.css';
+
+const SearchResult = (props) => {
+    console.log(props);
+    return (
+        <article className={classes.Result}>
+            <h1>{props.title}</h1>
+            <h3 className={classes.Author}>
+                {props.authors && props.authors.map((author, index) => (
+                    <p key={index}>{author}</p>
+                ))}
+            </h3>
+            <a href={props.link} rel="noopener noreferrer" target="_blank">{props.link}</a>
+        </article>
+    );
+}
+
+export default SearchResult;
