4a395fad5e7c21a27c2dce9a576b46bf255d8243
Anurup Joseph <anurup.joseph@jointheleague.org>
2020-10-21 18:27:16 -0400

    Using new component to display search results.

--- src/components/SearchOutput/SearchOutput.js
+++ src/components/SearchOutput/SearchOutput.js
@@ -1,4 +1,5 @@
 import React from "react";
+import SearchResult from "./SearchResult/SearchResult";
 
 const SearchOutput = (props) => {
     let message = null;
@@ -12,23 +13,29 @@ const SearchOutput = (props) => {
     if (props.results) {
         searchResults =
             props.results.map((result, index) => {
-                return <li key={index}>
-                    <a href={result.link}>{result.title}</a>
-                    {result.authors &&
-                        result.authors.map((author, index) => (
-                        <p key={index}>{author}</p>
-                        ))
-                    }
-                </li>
+                return (
+                    <SearchResult
+                        key={index}
+                        title={result.title}
+                        authors={result.authors}
+                        link={result.link}
+                    />
+                );
+                // return <li key={index}>
+                //     <a href={result.link}>{result.title}</a>
+                //     {result.authors &&
+                //         result.authors.map((author, index) => (
+                //         <p key={index}>{author}</p>
+                //         ))
+                //     }
+                // </li>
             })
     }
 
     return (
         <div>
             {message}
-            <ul>
-                {searchResults}
-            </ul>
+            {searchResults}
         </div>
     )
 }
